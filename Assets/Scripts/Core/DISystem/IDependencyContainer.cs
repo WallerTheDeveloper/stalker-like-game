55093a42fc9c9125c6819c7ba9b81e8b5a1e61de
Daniel <golo7ov.danil@gmail.com>
2024-02-06 00:05:48 +0100

    Small change - as reminder

--- Assets/Scripts/Core/DISystem/IDependencyContainer.cs
+++ Assets/Scripts/Core/DISystem/IDependencyContainer.cs
@@ -8,6 +8,7 @@ namespace Core.DISystem
             where TDependent : IDependentObject;
         void Deregister<T>(IDependency dependency, List<IDependency> linkedDependencies);
         
+        // TODO: Move to another interface - IDependencyProvider?
         T GetDependency<T>();
         
         void Tick(IDependency dependency);
