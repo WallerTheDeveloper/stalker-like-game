b8d3836ccb41949009b8b25a6b15169a1c7cde9e
Daniel <golo7ov.danil@gmail.com>
2024-02-05 23:48:58 +0100

    Changed Register/Deregister methods structure

--- Assets/Scripts/Core/DISystem/IDependencyContainer.cs
+++ Assets/Scripts/Core/DISystem/IDependencyContainer.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
+
 namespace Core.DISystem
 {
     public interface IDependencyContainer
     {
+        void Register<TDependent>(TDependent dependent, List<IDependency> linkedDependencies)
+            where TDependent : IDependentObject;
+        void Deregister<T>(IDependency dependency, List<IDependency> linkedDependencies);
+        
         T GetDependency<T>();
+        
         void Tick(IDependency dependency);
+        
         // void TickAll();
     }
 }
